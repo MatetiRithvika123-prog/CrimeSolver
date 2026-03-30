36223362f68179b64a3872a744caecf27e647b53
Mateti Rithvika <rithvikamateti@gmail.com>
2026-03-30 23:59:50 +0530

    Fixed duplicate path declaration

--- server/server.js
+++ server/server.js
@@ -15,9 +15,6 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Serve static files from client directory
-const path = require("path");
-
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "../client/login.html"));
 });
